f2d87a9929f81cec947c26bd8465af2a3a45735f
Rishabh Patil <rishabh@pensando.io>
2021-06-30 09:40:20 -0700

    list of apps to test

--- pyclient/utils/test_apps.py
+++ pyclient/utils/test_apps.py
@@ -5,13 +5,17 @@ print("Testing apps:\n")
 apps_directory = "../apps/"
 index = 0
 success = 0
-for i in os.listdir(apps_directory):
-    if i.endswith(".py"):
-        child = sp.Popen(["python3", apps_directory+i], stdout=sp.PIPE, stderr=sp.PIPE)
+
+apps_to_test = ["cluster_ping.py"]
+
+for app in os.listdir(apps_directory):
+    if app.endswith(".py") and app in apps_to_test:
+        index += 1
+        print("{0:<25} {1:<25}".format("Starting:", app))
+        child = sp.Popen(["python3", apps_directory+app], stdout=None, stderr=None)
         child.communicate()
         rc = child.returncode
-        index += 1
         success = success + (1 if rc == 0 else 0)
-        print("{0:<5} {1:<25} {2:<25}".format(index, i, "Success" if rc == 0 else "Failure"))
+        print("{0:<5} {1:<25} {2:<25}".format(index, app, "Success" if rc == 0 else "Failure"))
 
 print("\n{}/{} tests passed.".format(success, index))
